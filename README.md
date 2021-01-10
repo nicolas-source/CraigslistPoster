bfa1202971a3296bc7e115ce937b28b1a693ea2d
nicolas <nickzheng180@gmail.com>
2021-01-10 12:07:40 -0800

    Initial commit, poster works, gmail does not work

---
+++ README.md
@@ -0,0 +1,38 @@
+CraigLister
+===========
+
+This program will repeatedly post your craigslist ads every 72 hours and auto approve them.
+
+It is useful when you want something to sell put you don't want to repeatedly post them.
+
+It is built using selenium, it automates the browser actions and uses a gmail library to approve listings.
+
+Dependencies
+------------
+
+This program uses the gmail library found here:
+
+https://github.com/charlierguo/gmail/tree/master/gmail
+
+Also you need chromedriver as found here:
+
+http://chromedriver.storage.googleapis.com/index.html
+
+Pillow
+
+Python imaging library. To install use "pip install Pillow".
+If you have an error you may need to "pip uninstall PIL" and reinstall Pillow as well.
+
+Usage
+-----
+
+| Steps | Action |
+--------|--------|
+| 1 | Download the chromedriver and put into this directory |
+| 2 | Install and import the gmail library or download files from gmail link and put in this directory |
+| 3 | Change the gmail username/pass in the craiglister.py file
+| 4 | In the listings folder create a new folder that describes the item to post and include pictures and an info.txt file (There is already a "Laptop" folder as an example)|
+| 5 | Delete the Laptop Example in the listings folder and delete the 04-16-15 folder in the listed folder |
+| 6 | Run craigslister.py and it will post every listing in the listings folder |
+
+This script works by parsing the info.txt file, filling out the craigslist post with that information, uploading the pictures and then submitting the post. Craigslist will then email you an approval link, the gmail library will parse that link and selenium will direct you to that link and click accept. Then the folder you created for that post will get moved to a posted folder within a date folder. Then when it has been 72 hours or greater the posts within that date folder will be moved back to the posts pool and will get posted again.
---
+++ craiglister.py
@@ -0,0 +1,290 @@
+from gmail import Gmail
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
+import time
+import datetime
+import os
+import shutil
+from inspect import getsourcefile
+from os.path import abspath
+from gmail import Gmail
+# import gmail
+from datetime import date
+from PIL import Image
+
+gmailUser = "jackchen1526@gmail.com"
+gmailPass = "craigslistautomator"
+
+# --------------------------------------- Importing Stuff ----------------------
+
+file_path = abspath(getsourcefile(lambda _: None))
+file_dir = os.path.normpath(file_path + os.sep + os.pardir)
+listingsFolderDirectory = os.path.abspath(os.path.join(file_dir, "listings"))
+listedFolderDirectory = os.path.join(listingsFolderDirectory, "listed")
+chromedriver = file_dir + "/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+
+
+# ------------------------------- Set Up Necessary Directories ---------
+
+class listingInfoParse(object):
+    def __init__(self, f):
+        self.loc = parsing(f, "<Location>").lower()
+        self.title = parsing(f, "<Title>")
+        self.type = parsing(f, "<Type>")
+        self.category = parsing(f, "<Category>")
+        self.email = parsing(f, "<Email>")
+        self.street = parsing(f, "<Street>")
+        self.city = parsing(f, "<City>")
+        self.xstreet = parsing(f, "<CrossStreet>")
+        self.state = parsing(f, "<State>")
+        self.postal = parsing(f, "<Postal>")
+        self.body = parsing(f, "<Body>")
+        # just get rid of everything that not unicode
+        self.body = ''.join([i if ord(i) < 128 else '' for i in self.body])
+        # tabs will actually go to the next field in craiglist
+        self.body = " ".join(self.body.split("\t"))
+        self.price = parsing(f, "<Price>")
+
+
+# ------------------------------  Driver Navigation -----------------
+
+def clickDoneOnImageUploading(listing):
+    # listing.driver.find_element_by_xpath("//*[@id='pagecontainer']/section/form/button").click()
+    # listing.driver.find_element_by_link_text("done with images").click()
+    listing.driver.find_element_by_xpath("/html/body/article/section/form/button").click()
+
+
+# Don't always have to do this
+def clickAbideByGuidelines(listing):
+    try:
+        listing.driver.find_element_by_xpath("//*[@id='pagecontainer']/section/div/form/button").click()
+    except:
+        pass
+
+
+def clickClassImageUploader(listing):
+    listing.driver.find_element_by_id("classic").click()
+
+
+def clickListingType(listing):
+    # listing.driver.find_element_by_xpath(
+    #     "//*[@id='pagecontainer']/section/form//label[contains(.,'" + listing.type + "')]/input").click()
+    listing.driver.find_element_by_xpath(
+        "/html/body/article/section/form/ul/li[6]/label/span[1]/input").click()
+
+
+def clickListingCategory(listing):
+    # listing.driver.find_element_by_xpath(
+    #     "//*[@id='pagecontainer']/section/form/blockquote//label[contains(.,'" + listing.category + "')]/input").click()
+    listing.driver.find_element_by_xpath(
+        "/html/body/article/section/form/div/div/label/label[21]/input").click()
+
+
+def uploadImagePath(listing, image):
+    listing.driver.find_element_by_xpath(".//*[@id='uploader']/form/input[3]").send_keys(image)
+
+
+def fillOutListing(listing):
+    listing.driver.find_element_by_name("FromEMail").send_keys(listing.email)
+    # listing.driver.find_element_by_id("ConfirmEMail").send_keys(listing.email)
+    listing.driver.find_element_by_name("PostingTitle").send_keys(listing.title)
+    listing.driver.find_element_by_id("postal_code").send_keys(listing.postal)
+    listing.driver.find_element_by_name("PostingBody").send_keys(listing.body)
+    listing.driver.find_element_by_name("price").send_keys(listing.price)
+    # listing.driver.find_element_by_xpath("//*[@id='postingForm']/button").click()
+    listing.driver.find_element_by_name("go").click()
+
+def fillOutGeolocation(listing):
+    time.sleep(3)
+    listing.driver.find_element_by_id("xstreet0").send_keys(listing.street)
+    listing.driver.find_element_by_id("xstreet1").send_keys(listing.xstreet)
+    listing.driver.find_element_by_id("city").send_keys(listing.city)
+    # listing.driver.find_element_by_id("region").send_keys(listing.state)
+    time.sleep(1)
+    listing.driver.find_element_by_id("search_button").click()
+    time.sleep(2)
+    # listing.driver.find_element_by_id("postal_code").send_keys(postal) #Should already be there
+    listing.driver.find_element_by_xpath("//*[@id='leafletForm']/button[1]").click()
+
+
+def removeImgExifData(path):
+    filename, extension = os.path.splitext(path)
+    fullFilename = filename + extension
+    image = Image.open(fullFilename)
+    data = list(image.getdata())
+    imageNoExif = Image.new(image.mode, image.size)
+    imageNoExif.putdata(data)
+    imageNoExif.save(filename + "copy" + extension)
+    os.remove(filename + extension)
+    os.rename(filename + "copy" + extension, fullFilename)
+
+
+def uploadListingImages(listing):
+    clickClassImageUploader(listing)
+    for image in listing.images:
+        removeImgExifData(image)
+        uploadImagePath(listing, image)
+        time.sleep(2)
+    clickDoneOnImageUploading(listing)
+
+
+def clickAcceptTerms(listing):
+    listing.driver.find_element_by_xpath(
+        "//*[@id='pagecontainer']/section/section[1]//button[contains(.,'ACCEPT the terms of use')]").click()
+
+
+def clickPublishListing(listing):
+    # listing.driver.find_element_by_xpath(
+    #     "//*[@id='pagecontainer']/section/div[1]/form/button[contains(.,'publish')]").click()
+    listing.driver.find_element_by_xpath("/html/body/article/section/div[1]/form/button").click()
+
+
+def clickVancouverSubArea(listing):
+    listing.driver.find_element_by_xpath("/html/body/article/section/form/ul/li[1]/label/input").click()
+
+
+def postListing(listing):
+    clickVancouverSubArea(listing)
+    clickListingType(listing)
+    clickListingCategory(listing)
+    clickAbideByGuidelines(listing)
+    fillOutListing(listing)
+    fillOutGeolocation(listing)
+    uploadListingImages(listing)
+    clickPublishListing(listing)
+
+
+# --------------------------- Emails ---------------------
+
+def getFirstCraigslistEmailUrl(listing, emails):
+    for email in emails:
+        email.fetch()
+        email.read()
+        if listing.title[0:15] in email.subject:
+            emailMessage = email.body
+            email.archive()
+            acceptTermsLink = emailMessage.split("https")
+            acceptTermsLink = acceptTermsLink[1].split("\r\n")
+            return acceptTermsLink[0]
+
+
+def acceptTermsAndConditions(listing, termsUrl):
+    listing.driver.get("https" + termsUrl)
+    clickAcceptTerms(listing)
+
+
+def acceptEmailTerms(listing):
+    # gmail = GMail()
+    # gmail.login(gmailUser, gmailPass)
+    g = Gmail(gmailUser, gmailPass)
+    g.login(gmailUser,gmailPass)
+
+    today = date.today()
+    year = today.year
+    month = today.month
+    day = today.day
+
+    time.sleep(120)
+    print("Checking email")
+    emails = g.inbox().mail(sender="robot@craigslist.org", unread=True, after=datetime.date(year, month, day - 1))
+    termsUrl = getFirstCraigslistEmailUrl(listing, emails)
+    acceptTermsAndConditions(listing, termsUrl)
+
+    g.logout()
+    print("Done Checking Email")
+
+
+# --------------------------- Craigslist Posting Actions ---------------
+
+def moveFolder(folder, listedFolderDirectory):
+    now = time.strftime("%c")
+
+    # %x >>>get the date like this 7/16/2014
+    today_dir = os.path.join(listedFolderDirectory, time.strftime("%x").replace("/", "-"))
+
+    # Make todays date under the listed directory
+    # makeFolder(today_dir)
+    os.mkdir(today_dir)
+    # Move the folder to the listed todays date directory
+    shutil.move(folder, today_dir)
+
+
+def parsing(f, splits):
+    fsplit = f.split(splits)
+    return fsplit[1]
+
+
+# If more than 24 hours passed will look like
+# 1 day, 13:37:47.356000
+def hasItBeenXDaysSinceFolderListed(folder, x):
+    dateSplit = folder.split('-')
+    folderDate = datetime.date(int(dateSplit[2]) + 2000, int(dateSplit[0]), int(dateSplit[1]))
+    currentDatetime = datetime.datetime.now()
+    folderTimePassed = currentDatetime - datetime.datetime.combine(folderDate, datetime.time())
+    if "day" not in str(folderTimePassed):
+        return False
+    daysPassed = str(folderTimePassed).split('day')[0]
+    if int(daysPassed.strip()) >= x:
+        return True
+    return False
+
+
+def getOrderedListingImages(listingFolder):
+    print('listingFolder', listingFolder)
+    listingImages = [f for f in os.listdir(listingFolder) if
+                     os.path.isfile(os.path.join(listingFolder, f)) and f[0] != '.' and f != 'info.txt']
+    print('listingImages', listingImages)
+    secondList = [os.path.abspath(os.path.join(listingFolder, x)) for x in listingImages if
+                  (x[1] != "_") or (x[0].isdigit() == False) and x[0] != '.']
+    firstList = [os.path.abspath(os.path.join(listingFolder, x)) for x in listingImages if
+                 (x[1] == "_") and (x[0].isdigit()) and x[0] != '.']
+
+    firstList.sort()
+    secondList.sort()
+
+    orderedListingImages = []
+    for x in firstList: orderedListingImages.append(x)
+    for x in secondList: orderedListingImages.append(x)
+    return orderedListingImages
+
+
+
+# Get all the date folders of listed items
+listedItemsFolders = [folder for folder in os.listdir(listedFolderDirectory) if folder[0] != "."]
+
+# Moving items that are 3 days or older back into the queue to get listed again
+for dayListedFolder in listedItemsFolders:
+
+    if (hasItBeenXDaysSinceFolderListed(dayListedFolder, 3) == False):
+        continue
+
+    listedFolders = [listedFolders for listedFolders in os.listdir(os.path.join(listedFolderDirectory, dayListedFolder))
+                     if listedFolders[0] != "."]
+    dayListedFolderDirectory = os.path.join(listedFolderDirectory, dayListedFolder)
+
+    for listedFolder in listedFolders:
+        theListedFolderDirectory = os.path.join(dayListedFolderDirectory, listedFolder)
+        shutil.move(theListedFolderDirectory, listingsFolderDirectory)
+    shutil.rmtree(dayListedFolderDirectory)
+
+# List Items
+listingFolders = [listing for listing in os.listdir(listingsFolderDirectory) if
+                  listing[0] != "." and listing != "listed"]
+
+for listingFolder in listingFolders:
+    listingFolder = os.path.abspath(os.path.join(listingsFolderDirectory, listingFolder))
+    with open(os.path.abspath(os.path.join(listingFolder, 'info.txt')), 'r') as info:
+        listing = listingInfoParse(info.read())
+    listing.images = getOrderedListingImages(listingFolder)
+    listing.driver = webdriver.Chrome(chromedriver)
+    listing.driver.get("https://post.craigslist.org/c/" + listing.loc + "?lang=en")
+    # postListing(listing)
+    acceptEmailTerms(listing)
+    moveFolder(listingFolder, listedFolderDirectory)
+    listing.driver.close()
+    time.sleep(120)
+    print("Waiting 2 minutes")
+print("No More Craiglist Items To List")
